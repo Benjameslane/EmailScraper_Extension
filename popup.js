0e2a3209e819c283b542b8c283eb3a6eb622d3aa
Benjameslane <ben.james.lane9>
2024-01-26 16:15:00 -0700

    added organization to the popup via addListener

--- popup.js
+++ popup.js
@@ -1,5 +1,32 @@
 let scrapeEmails = document.getElementById('scrapeEmails');
+let list = document.getElementById('emailList');
 
+// Handler to recieve email;s from content script
+chrome.runtime.onMessage.addListener((request,
+    sender,sendResponse) => {
+
+        // Get Emails
+        let emails = request.emails;
+
+        // Display emails on popup
+        if(emails ==null || emails.length == 0) {
+            // No Emails
+            let li = document.createElement('li');
+            li.innerText = "No Emails Found";
+            list.appendChild(li);
+
+        } else {
+            //Display Emails
+            emails.forEach((email) => {
+            let li = document.createElement('li');
+            li.innerText = email;
+            list.appendChild(li);
+            });
+        }
+
+    });
+
+    //Button's click event listener
 scrapeEmails.addEventListener("click", async () => {
 
     // Get current active tab on Chrome Window
@@ -23,5 +50,8 @@ function scrapeEmailsFromPage() {
     let emails = document.body.innerHTML.match
     (emailRegEx);
 
-    alert(emails);
+    // send emails to popup
+    chrome.runtime.sendMessage({emails});
+
+
 }
